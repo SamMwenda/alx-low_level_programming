93d7cc7a2cb187c5acced52471e6640ebe4f4772
thatguyskullie <skmwenda47@gmail.com>
2022-06-02 03:00:06 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * 4-puts.c
+ *  A program that prints with puts instead of printf
+ */
+#include <stdio.h>
+
+/**
+ * main - My entry point for my program
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
