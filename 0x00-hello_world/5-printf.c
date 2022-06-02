0898b619d86a01c5ca46ba1ef4e3b2dc01173afd
thatguyskullie <skmwenda47@gmail.com>
2022-06-02 03:18:29 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ *  5-printf.c
+ *  A program that prints with puts instead of printf
+ */
+#include <stdio.h>
+
+/**
+ * main - My entry point for my program
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+		puts("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
