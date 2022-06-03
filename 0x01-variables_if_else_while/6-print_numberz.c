f8cd79b528f32fb43019792acf28075c6876fe80
thatguyskullie <skmwenda47@gmail.com>
2022-06-03 06:16:34 -0700

    Completed Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+/*
+ *  6-print_numberz.c
+ *  A program that prints all numbers from 0 to 9
+ *  Using putchar
+ *
+ *  Auth: thatguyskullie
+ */
+ 
+#include <stdio.h>
+
+/**
+ * main - My program entry point
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{	
+		putchar('0' + i);
+		
+	}
+	putchar('\n');
+	return (0);
+}
