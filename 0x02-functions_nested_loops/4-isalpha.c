f6b5e0d547b878f6fcdac5deb2e2bc94c50ccc9d
thatguyskullie <skmwenda47@gmail.com>
2022-06-07 12:16:01 -0700

    Fixed Completed Task 4 + cleaned up file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is upper case
+ * _isalpha - checks if a character is upper case
  * @c: The chracter to check
  * Return: 1 if uppercase 0 otherwise
  */
-int _isupper(char c)
+int _isalpha(char c)
 {
 	if (c < 65 || c > 90)
 	{
