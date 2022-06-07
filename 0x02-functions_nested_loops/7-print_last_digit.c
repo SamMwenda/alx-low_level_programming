45b8d4042795be4a63c01ca948b4794ace37fb86
Sammy Mwenda <skmwenda47@gmail.com>
2022-06-08 00:23:52 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,7 @@ int print_last_digit(int a)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == (a + '0'))
-		{
-			_putchar(i);
-			break;
-		}
-		else
-		{
-			continue;
-		}
+		putchar((a % 10) + '0');
 	}
 	return (a % 10);
 }
