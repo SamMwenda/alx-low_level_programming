a8f5d6a21845f44d8e9af922073e5c4f857b0f17
thatguyskullie <skmwenda47@gmail.com>
2022-06-07 06:29:41 -0700

    Completed Task 1

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints a to z in lowercase
+ * 
+ */
+void print_alphabet(void)
+{
+	char alphabets;
+	
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+	_putchar('\n');
+}
