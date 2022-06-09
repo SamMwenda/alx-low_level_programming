d5e0636bdb238e21b944b791d59406dd40b7b389
thatguykullie <murumek47@gmail.com>
2022-06-09 19:39:17 +0300

    Updated 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -1,44 +1,80 @@
-#include <unistd.h>
 #include <stdio.h>
-//#include "main.h"
+#include <unistd.h>
+#include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
     return (write(1, &c, 1));
 }
 
-int largest_number(int a, int b, int c)
-{
-    int largest;
-
-    if (a > b || b > c)
-    {
-        largest = a;
-    }
-    else if (b > a || a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
-
+/**
+ * print_number - Print any number one character at a time
+ * Description: The program is to test the files of
+ * 0x04-more_functions_nested_loops
+ * Return: Always 0
+ */
 int main(void)
 {
-    int a, b, c;
-    int largest;
+    /**
+     * Tests 0-isupper.c
+     * 
+     * char c;
+     *
+     * c = 'A';
+     * printf("%c: %d\n", c, _isupper(c));
+     * c = 'a';
+     * printf("%c: %d\n", c, _isupper(c));
+     * return (0);
+     */
+    
+    /**
+     * Tests 1-isdigit.c
+     * 
+     * char c;
+     * 
+     * c = '0';
+     * printf("%c: %d\n", c, _isdigit(c));
+     * c = 'a';
+     * printf("%c: %d\n", c, _isdigit(c));
+     * return (0);
+     */
 
-    a = 3;
-    b = 2;
-    c = 0;
+    /**
+     * Tests 2-mul.c
+     * 
+     * printf("%d\n", mul(98, 1024));
+     * printf("%d\n", mul(-402, 4096));
+     * return (0);
+    */
+    
+    /**
+     * Tests 3-print_numbers.c
+     * 
+     * print_numbers();
+     * return (0);
+     */
+    /**
+     * Tests 4-print_most_numbers.c
+     * 
+     * print_most_numbers();
+     * return (0);
+     */
+    
+     /* To be continued..... */
 
-    largest = largest_number(a, b, c);
-
-    printf("%d is the largest number\n", largest);
-
-    return (0);
+    /* Tests 5-more_numbers.c */
+    /* Tests 6-print_line.c */
+    /* Tests 7-print_diagonal.c */
+    /* Tests 8-print_square.c */
+    /* Already tested 9-fizz_buzz. */
+    /* Tests 10-print_triangle.c */
+    /* Tests 100-prime_factor.c */
+    /* Tests 101-print_number.c */
 }
